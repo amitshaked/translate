749f7886a53cc14f768b60c986edb4bd517c06e5
amitshaked <amit.shaked1@gmail.com>
2015-07-22 22:58:40 +0300

    added giza project and using it for word alignment

--- phrase_table.py
+++ phrase_table.py
@@ -1,4 +1,7 @@
 import re
+import os.path
+import subprocess
+
 
 ALPHABET = [chr(ord('a') + i) for i in xrange(ord('z') - ord('a') + 1)] + [chr(ord('A') + i) for i in xrange(ord('Z') - ord('A') + 1)]
 
@@ -13,7 +16,7 @@ def tokenize(s):
 
 class PhraseTable(object):
 
-	def __init__(self, src_path, dest_path, word_output, phrase_output, verbose=True):
+	def __init__(self, source_language_corpus_path, target_language_corpus_path, alignment_folder, word_output, phrase_output, max_tokens, verbose=True):
 		# Print to log if needed
 		if verbose:
 			def info(s):
@@ -23,37 +26,59 @@ class PhraseTable(object):
 				pass
 
 		self.info = info
-
-		self.src_path = src_path
-		self.dest_path = dest_path
-
+		self.source_language_corpus_path = source_language_corpus_path
+		self.target_language_corpus_path = target_language_corpus_path
+		self.max_tokens = max_tokens
+		self.alignment_folder = alignment_folder
 		self.word_output = word_output
 		self.phrase_output = phrase_output
 
-
-
-	def clean(self, m):
-		
+	def _clean(self, src, dest, cleaned_src_path, cleaned_target_path, m):
 		def longer_than_max(x):
 			return len(tokenize(x)) < m
 
 		cleaned_src = []
-		cleaned_dest = []
-
+		cleaned_target = []
+		
 		self.info('Cleaning...')
-		with open(self.src_path, 'rb') as src:
+		with open(self.source_language_corpus_path, 'rb') as src:
 			cleaned_src = filter(longer_than_max, src.readlines())
-		with open(self.dest_path, 'rb') as dest:
-			cleaned_dest = filter(longer_than_max, dest.readlines())
+		with open(self.target_language_corpus_path, 'rb') as target:
+			cleaned_target = filter(longer_than_max, target.readlines())
 
 		self.info('Dumping cleaned files...')
-		with open(self.src_path + '.cleaned', 'wb') as src:
+		with open(cleaned_src_path, 'wb') as src:
 			src.writelines(cleaned_src)
-		with open(self.dest_path + '.cleaned', 'wb') as dest:
-			dest.writelines(cleaned_dest)
+		with open(cleaned_target_path, 'wb') as target:
+			target.writelines(cleaned_target)
 
-	def word_alignment(self):
-		pass
+	def word_alignment(self, override=False):
+		final_path = self.alignment_folder + '/' + self.word_output + '.actual.ti.final'
+		if os.path.isfile(final_path) and not override:
+			return
+
+		#Clean lines longer than max tokens
+		cleaned_src_path = self.source_language_corpus_path + '.cleaned'
+		cleaned_target_path = self.target_language_corpus_path + '.cleaned'
+		self._clean(self.source_language_corpus_path, self.target_language_corpus_path, cleaned_src_path, cleaned_target_path, self.max_tokens)
+
+		#Create snt files
+		self.info('Create snt files...')
+		subprocess.call([r'giza-pp/GIZA++-v2/plain2snt.out', cleaned_src_path, cleaned_target_path])
+
+		#Create vcb.classes files
+		self.info('Create classes files...')
+		subprocess.call([r'giza-pp/mkcls-v2/mkcls', '-p' + cleaned_src_path, '-V' + cleaned_target_path + '.vcb.classes'])
+		subprocess.call([r'giza-pp/mkcls-v2/mkcls', '-p' + cleaned_target_path, '-V' + cleaned_src_path + '.vcb.classes'])
+
+		#Run word alignment
+		target_source_snt = cleaned_target_path + '_' + cleaned_src_path.split('/')[-1] + '.snt'
+		self.info('Running word alinment...')
+		subprocess.call([r'giza-pp/GIZA++', ' -S ' + cleaned_target_path + '.vcb' \
+			,' -T ' + cleaned_src_path + '.vcb' \
+			,' -C ' + target_source_snt \
+			,' -o ' + self.word_output \
+			,' -outputpath ' + self.alignment_folder])
 
 	def phrase_alignment(self):
 		pass
--- train.py
+++ train.py
@@ -4,23 +4,24 @@ import phrase_table
 from collections import namedtuple
 
 def main():
-    parser = argparse.ArgumentParser(description="Build a phrase table from the bilingual parallel texts and a language model from the corpus file")
-    parser.add_argument('-s', '--src', type=str, required=True, help='The source language file')
-    parser.add_argument('-d', '--dest', type=str, required=True, help='The destination language file')
-    parser.add_argument('-c', '--corpus', type=str, required=True, help='The corpus file')
-    parser.add_argument('-po', '--phrase_output', type=str, required=True, help='phrase alignment output path')
-    parser.add_argument('-lo', '--lm_output', type=str, required=True, help='language model output path')
-    parser.add_argument('-wo', '--word_output', type=str, default='./words.A3', help='word alignment output path')
-    parser.add_argument('-mt', '--max_tokens', type=int, default=60, help='Maximum number of tokens pe line')
-   
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description="Build a phrase table from the bilingual parallel texts and a language model from the corpus file")
+	parser.add_argument('-s', '--source_language_corpus_path', type=str, required=True, help='The source language file')
+	parser.add_argument('-d', '--target_language_corpus_path', type=str, required=True, help='The destination language file')
+	parser.add_argument('-c', '--corpus_file_for_lm', type=str, required=True, help='The corpus file for building a language model')
+	parser.add_argument('-af', '--alignment_folder', type=str, required=True, help='alignment folder')
+	parser.add_argument('-wo', '--word_output', type=str, default='word', help='word alignment output path')
+	parser.add_argument('-po', '--phrase_output', type=str, default='phrase', help='phrase alignment output path')
+	parser.add_argument('-mt', '--max_tokens', type=int, default=60, help='Maximum number of tokens pe line')
+	parser.add_argument('-lo', '--lm_output', type=str, required=True, help='language model output path')
 
-    pt = phrase_table.PhraseTable(args.src, args.dest, args.word_output, args.phrase_output)
-    pt.clean(args.max_tokens)
-    pt.word_alignment()
-    pt.phrase_alignment()
-    
-    return 0
+	args = parser.parse_args()
+
+	pt = phrase_table.PhraseTable(args.source_language_corpus_path, args.target_language_corpus_path, \
+		args.alignment_folder, args.word_output, args.phrase_output, args.max_tokens, verbose = True)
+	pt.word_alignment()
+	pt.phrase_alignment()
+
+	return 0
 
 if __name__ == '__main__':
-    sys.exit(main())
+	sys.exit(main())
