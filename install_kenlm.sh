2bdd41fadb178638471156caf510b7fff2fca098
oranav <oranav@gmail.com>
2015-08-24 18:38:53 +0300

    Fix kenlm build on nova

--- install_kenlm.sh
+++ install_kenlm.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+mkdir externals
 cd externals
 wget -O - http://kheafield.com/code/kenlm.tar.gz |tar xz
 cd kenlm
-./bjam -j12
+bjam -j12 linkflags=-lboost_thread-mt
 python setup.py build
 cd ../..
 ln -s externals/kenlm/build/lib.*/kenlm.so kenlm.so
